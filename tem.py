7ef589d8d3071fda41e5b58b43af1f289bf7cf32
samarthdandin866-star <samarthdandin866@gmail.com>
2025-11-11 10:36:47 +0530

    Add temperature state classification script

---
+++ tem.py
@@ -0,0 +1,18 @@
+import sys
+if len(sys.argv) ==2:
+    script_name=sys.argv[0]
+    temp=sys.argv[1]
+    print("user provided inputs")
+else:
+    script_name=sys.argv[0]
+    temp="40"
+    print("default inputs")
+if temp<"15":
+    state="cold"
+elif temp<"15"and temp>"30":
+    state="normal"
+else:
+    state="hot"
+print("Script Name:",script_name)
+print("Temperature:",temp)
+print("State:",state)
